675915fdf2591c98fc4e4f059dff7ea8886d1000
juice <jmcclusky17@gmail.com>
2017-12-28 21:01:23 -0500

    Added code to auto open app in web browser.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from validate_input import validate_input
+import webbrowser
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -16,5 +17,21 @@ def signup():
     email = request.form['email']
     
     return validate_input(username, password, verify, email)
-   
-app.run()
+
+# adapted from this stack overflow answer
+# https://stackoverflow.com/a/24353812/7542831
+port = 5000 # set the port you want to expose here
+url = 'http://127.0.0.1:{}'.format(port)
+
+# these are the file paths to the chrome / other browser you want to auto-open:
+# MacOS
+#chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+# Windows (uncomment for windows machines)
+# chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
+# Linux (uncomment for linux machines)
+browser_path = '/usr/bin/firefox %s'
+
+# this calls the webbrowser module to open the url with the given browser
+webbrowser.get(browser_path).open(url)
+
+app.run(port=port)
